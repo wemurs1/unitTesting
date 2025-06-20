757c1d3861f83d640bd5448535c72d405fee1dca
Stuart Murray <stuart.murray.dev@outlook.com>
2025-06-20 13:40:51 +0100

    complete coverage of TreasureChest

--- GameLibrary/GameLibrary.UnitTests/TreasureChestTests.cs
+++ GameLibrary/GameLibrary.UnitTests/TreasureChestTests.cs
@@ -8,10 +8,49 @@ public class TreasureChestTests
     public void CanOpen_ChestIsLockedAndHasKey_ReturnsTrue()
     {
         // Arrange
-        var chest = new TreasureChest(true);
+        var sut = new TreasureChest(true);
 
         // Act
-        var result = chest.CanOpen(true);
+        var result = sut.CanOpen(true);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void CanOpen_ChestIsLockedAndHasNoKey_ReturnsFalse()
+    {
+        // Arrange
+        var sut = new TreasureChest(true);
+
+        // Act
+        var result = sut.CanOpen(false);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void CanOpen_ChestIsUnLockedAndHasKey_ReturnsTrue()
+    {
+        // Arrange
+        var sut = new TreasureChest(false);
+
+        // Act
+        var result = sut.CanOpen(true);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void CanOpen_ChestIsUnLockedAndHasNoKey_ReturnsTrue()
+    {
+        // Arrange
+        var sut = new TreasureChest(false);
+
+        // Act
+        var result = sut.CanOpen(false);
 
         // Assert
         Assert.True(result);
