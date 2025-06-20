360d3fd7ac34bf66edc31e4d93fac0d5971dc1de
Stuart Murray <stuart.murray.dev@outlook.com>
2025-06-20 08:34:41 +0100

    init

---
+++ GameLibrary/GameLibrary/GameWorld.cs
@@ -0,0 +1,34 @@
+﻿namespace GameLibrary;
+
+public class GameWorld
+{
+    private readonly FileBasedPlayerStatisticsService playerStatisticsService;
+
+    public GameWorld()
+    {
+        playerStatisticsService = new("statistics.json");
+    }
+
+    public PlayerReportDto GetPlayerReport(Player player)
+    {
+        var stats = playerStatisticsService.GetPlayerStatistics(player.Name);
+        double averageScore = stats.GamesPlayed == 0 ? 0 : (double)stats.TotalScore / stats.GamesPlayed;
+
+        return new PlayerReportDto(
+            player.Name,
+            player.Level,
+            player.JoinDate,
+            stats.GamesPlayed,
+            stats.TotalScore,
+            averageScore
+        );
+    }
+
+    public void RecordPlayerGameWin(Player player, int score)
+    {
+        var stats = playerStatisticsService.GetPlayerStatistics(player.Name);
+        stats.TotalScore += score;
+        stats.GamesPlayed += 1;
+        playerStatisticsService.UpdatePlayerStatistics(stats);
+    }    
+}
---
+++ GameLibrary/GameLibrary/TreasureChest.cs
@@ -0,0 +1,23 @@
+﻿namespace GameLibrary;
+
+public class TreasureChest
+{
+    public TreasureChest(bool isLocked)
+    {
+        IsLocked = isLocked;
+    }
+
+    public bool IsLocked { get; set; }
+
+    public bool CanOpen(bool hasKey)
+    {
+        if (IsLocked && !hasKey)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
